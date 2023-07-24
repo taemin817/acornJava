21c7d1ca818c5e818cd5dfe01fe8147605c1fd0b
taemin817 <tmpark11@naver.com>
2023-07-24 10:56:55 +0900

    up

---
+++ javaPrj/src/day4/TenEx.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class TenEx {
+
+	public static void main(String[] args) {
+		// 10~1까지의 합
+				int sum=0;
+				for(int i = 10; i>0; i--) {
+					sum+=i;
+					System.out.println(sum);
+				}
+				System.out.println(sum);
+				System.out.println();
+				
+	}
+
+}
