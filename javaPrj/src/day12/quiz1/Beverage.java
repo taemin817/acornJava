d1a28950bd57db950ea5de404791970456993516
taemin817 <tmpark11@naver.com>
2023-08-02 17:48:56 +0900

    상속...

--- javaPrj/src/day12/quiz1/Beverage.java
+++ javaPrj/src/day12/quiz1/Beverage.java
@@ -1,17 +1,5 @@
 package day12.quiz1;
 
 public class Beverage {
-	String 아아;
-	String 탄산수;
-	String 물;
-	String 우유;
-	
-	public Beverage(String 아아, String 탄산수, String 물, String 우유) {
-		super();
-		this.아아 = 아아;
-		this.탄산수 = 탄산수;
-		this.물 = 물;
-		this.우유 = 우유;
-	}
 	
 }
